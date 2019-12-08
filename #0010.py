415242517c789ffb1402794aea5d9ce0136bdd46
Sidhant Kumar Nayak <macyapp@users.noreply.github.com>
2019-12-08 17:48:22 +0530

    Update #0010.py

--- #0010.py
+++ #0010.py
@@ -4,10 +4,12 @@ n=2000000
 sum=0
 print("The sum is:")
 for i in range(1,n+1):
-    count=0
+    count=0            #No. of factors for the number 'i'
     for j in range(1,i+1):
         if i%j==0:
             count+=1
+        if count>2:    #This line breaks the loop if there are more than 2 factors for a number
+            break
     if count==2:
         sum+=i
-        print(sum)
+        print(i," ",sum)
